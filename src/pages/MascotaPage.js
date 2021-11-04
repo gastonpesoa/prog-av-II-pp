43d189acd5de5e07b0f793a28d157b51694937ed
Gaston Pesoa <gaston.pesoa@globant.com>
2021-11-03 22:03:20 -0300

    fixes

--- src/pages/MascotaPage.js
+++ src/pages/MascotaPage.js
@@ -1,48 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { Link } from 'react-router-dom';
+import Loader from '../components/Loader'
 
 const MascotaPage = () => {
 
+    const [loading, setloading] = useState(false)
     const { id } = useParams();
     const [mascota, setMascota] = useState({})
     const { nombre, edad, tipo, vacunado, observaciones } = mascota;
 
     useEffect(() => {
+        setloading(true);
         const URL = "http://localhost:5000/mascotas/";
         fetch(`${URL}/${id}`)
             .then(res => res.ok ? res.json() : Promise.reject(res.status + ":" + res.statusText))
             .then(data => {
                 console.log(data)
                 setMascota(data)
+                setloading(false);
             })
             .catch((error) => {
                 console.error(error)
+                setloading(false);
             })
     }, [id])
 
     return (
         <section className="section">
-            <div className="container">
-                <Link to="/"><button class="button is-rounded">Volver</button></Link>
-                <div className="card">
-                    <div className="card-content">
-                        <div className="media">
-                            <div className="media-content">
-                                <p className="title is-4"> {nombre}</p>
-                                <p className="subtitle is-6">Edad: {edad}</p>
-                                <p className="subtitle is-6">{tipo}</p>
-                                <p className="subtitle is-6">Vacunado: {vacunado ? "SI" : "NO"}</p>
+        {
+            loading
+                ? (<Loader />)
+                : (
+                <div className="container">
+                    <Link to="/"><button className="button is-rounded">Volver</button></Link>
+                    <div className="card">
+                        <div className="card-content">
+                            <div className="media">
+                                <div className="media-content">
+                                    <p className="title is-4"> {nombre}</p>
+                                    <p className="subtitle is-6">Edad: {edad}</p>
+                                    <p className="subtitle is-6">{tipo}</p>
+                                    <p className="subtitle is-6">Vacunado: {vacunado ? "SI" : "NO"}</p>
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="content">
-                            {observaciones}
+    
+                            <div className="content">
+                                {observaciones}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+           )
+        }
+         </section>
     )
 }
 
